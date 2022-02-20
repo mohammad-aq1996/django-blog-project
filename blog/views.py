a56d32d4402316d6894d9c1f7448eb7b4d1ebeda
Mohammad <mohammad.aq1996@outlook.com>
2022-02-20 11:45:03 +0330

    chanfe blog to post in view.py

--- blog/views.py
+++ blog/views.py
@@ -4,15 +4,15 @@ from .models import Post, Category, Tag, Blogger, Comment
 from .forms import CommentForm
 
 
-class BlogListView(ListView):
+class PostListView(ListView):
     model = Post
     paginate_by = 4
     template_name = 'blog/blog.html'
-    context_object_name = 'blogs'
+    context_object_name = 'posts'
 
     def get_queryset(self):
-        blogs = Post.objects.all().order_by('-created_at')
-        return blogs
+        posts = Post.objects.all().order_by('-created_at')
+        return posts
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -23,42 +23,42 @@ class BlogListView(ListView):
         return context
 
 
-class BlogCategory(BlogListView):
+class PostCategory(PostListView):
     paginate_by = 0
 
     def get_context_data(self, **kwargs):
-        context = super(BlogCategory, self).get_context_data(**kwargs)
-        context['blogs'] = Post.objects.filter(category__slug=self.kwargs['slug'])
+        context = super(PostCategory, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(category__slug=self.kwargs['slug'])
         return context
 
 
-class SearchView(BlogListView):
+class SearchView(PostListView):
     paginate_by = 0
 
     def get_context_data(self, **kwargs):
         context = super(SearchView, self).get_context_data(**kwargs)
         q = self.request.GET.get('search')
-        context['blogs'] = Post.objects.filter(title__icontains=q)
+        context['posts'] = Post.objects.filter(title__icontains=q)
         return context
 
 
-class BlogTag(BlogListView):
+class PostTag(PostListView):
     paginate_by = 0
 
     def get_context_data(self, **kwargs):
-        context = super(BlogTag, self).get_context_data(**kwargs)
-        context['blogs'] = Post.objects.filter(tags__slug=self.kwargs['tag'])
+        context = super(PostTag, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(tags__slug=self.kwargs['tag'])
         return context
 
 
 def blog_detail_view(request, pk):
     context = dict()
-    context['blog'] = Post.objects.get(id=pk)
+    context['post'] = Post.objects.get(id=pk)
     context['tags'] = Tag.objects.all()
     context['categories'] = Category.objects.all()
     context['recent'] = Post.objects.all().order_by('-created_at')[:3]
     context['blogger'] = Blogger.objects.get(id=1)
-    context['blogs'] = Post.objects.all()
+    context['posts'] = Post.objects.all()
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -67,7 +67,7 @@ def blog_detail_view(request, pk):
             email = form.cleaned_data['email']
             title = form.cleaned_data['title']
             message = form.cleaned_data['message']
-            comment = Comment(blog=context['blog'],
+            comment = Comment(post=context['post'],
                               name=name,
                               email=email,
                               title=title,
