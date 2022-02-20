064cacd9c93ca2d22fa4a16968497dba088719e2
Mohammad <mohammad.aq1996@outlook.com>
2022-02-20 11:49:04 +0330

    rename in models. ja monde ha

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author', default=1)
     title = models.CharField(max_length=150)
     content = RichTextField()
-    image = models.ImageField(upload_to='blog')
+    image = models.ImageField(upload_to='post')
     category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category')
     tags = models.ManyToManyField('Tag', related_name='tags')
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
@@ -36,14 +36,14 @@ class Blogger(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
     describe = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='blog')
+    image = models.ImageField(upload_to='post')
 
     def __str__(self):
         return self.author.username
 
 
 class Comment(models.Model):
-    blog = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=150)
     email = models.EmailField()
     title = models.CharField(max_length=250)
