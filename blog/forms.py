430fc7cb2784b4bf1631dfc0b288556c50589a7f
Mohammad <mohammad.aq1996@outlook.com>
2022-02-23 23:48:10 +0330

    Commenting blog forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,10 @@ from .models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    """
+    Return a form for comment
+    This form will be used in posts detail view
+    """
     class Meta:
         model = Comment
         fields = ('name', 'email', 'title', 'message')
