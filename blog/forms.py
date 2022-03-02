71acdac1d7a0f292049f806d35f1cd5e47965713
Mohammad <mohammad.aq1996@outlook.com>
2022-03-03 01:01:58 +0330

    create form get better

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from .models import Comment, Post
+from .models import Comment, Post, Tag, Category
 from ckeditor.widgets import CKEditorWidget
+from django.contrib.auth.models import User
 
 
 class CommentForm(forms.ModelForm):
@@ -19,4 +20,16 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
+
         fields = ('author', 'title', 'content', 'image', 'category', 'tags')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'tags': forms.CheckboxSelectMultiple(),
+            'category': forms.RadioSelect(),
+        }
+        # category = forms.ModelChoiceField(queryset=Category.objects.all(), widget=forms.C)
+        # tags = forms.ModelMultipleChoiceField(
+        #     queryset=Tag.objects.all(),
+        #     widget=forms.widgets.CheckboxSelectMultiple()
+        # )
+        #
