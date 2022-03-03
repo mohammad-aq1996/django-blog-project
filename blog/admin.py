cc744a27ac091a2f17a2deaa0fd106786f2fb412
Mohammad <mohammad.aq1996@outlook.com>
2022-03-03 19:42:08 +0330

    add custom admin page for post, category and tags

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,32 @@
 from django.contrib import admin
 from .models import Post, Category, Blogger, Tag, Comment
 
-
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Blogger)
 admin.site.register(Comment)
+admin.site.register(Blogger)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['title', 'content']
+    raw_id_fields = ('author',)
+    # list_editable = ['status']
+    # prepopulated_fields = {'slug': ['title']}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    list_display_links = ['slug']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    list_display_links = ['slug']
+
+
+
--- blog/models.py
+++ blog/models.py
@@ -8,13 +8,17 @@ class Post(models.Model):
     """
     creating a table for blog post in database
     """
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author', default=1)
-    title = models.CharField(max_length=150)
-    content = RichTextField()
-    image = models.ImageField(upload_to='post')
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category')
-    tags = models.ManyToManyField('Tag', related_name='tags')
-    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author', verbose_name='نویسنده', default=1)
+    title = models.CharField(max_length=150, verbose_name='عنوان')
+    content = RichTextField(verbose_name='محتوا')
+    image = models.ImageField(upload_to='post', verbose_name='تصویر')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category', verbose_name='دسته بندی')
+    tags = models.ManyToManyField('Tag', related_name='tags', verbose_name='تگ ها')
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True, verbose_name='تاریخ انتشار')
+
+    class Meta:
+        verbose_name = 'پست'
+        verbose_name_plural = 'پست ها'
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', kwargs={'pk': self.id})
@@ -28,8 +32,12 @@ class Category(models.Model):
     creating a table for categories in database
     relation: category 1:1 post
     """
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, verbose_name='عنوان دسته بندی')
     slug = models.SlugField()
+
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
 
     def __str__(self):
         return self.name
@@ -40,8 +48,12 @@ class Tag(models.Model):
     creating a table for tags in database
     relation: many to many with post
     """
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, verbose_name='عنوان تگ')
     slug = models.SlugField()
+
+    class Meta:
+        verbose_name = 'تگ'
+        verbose_name_plural = 'تگ ها'
 
     def __str__(self):
         return self.name
@@ -52,10 +64,14 @@ class Blogger(models.Model):
     creating a table for blogger profile in database
     relation: blogger 1:1 user
     """
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    describe = models.CharField(max_length=100)
-    content = models.TextField()
-    image = models.ImageField(upload_to='post')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', verbose_name='نویسنده')
+    describe = models.CharField(max_length=100, verbose_name='عنوان')
+    content = RichTextField(verbose_name='توضیحات')
+    image = models.ImageField(upload_to='post', verbose_name='تصویر')
+
+    class Meta:
+        verbose_name = 'ادمین وبلاگ'
+        verbose_name_plural = 'ادمین وبلاگ'
 
     def __str__(self):
         return self.author.username
