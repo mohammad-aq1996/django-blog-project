2aac984fc3598e829fe062fde22ba4c5903325c1
Mohammad <mohammad.aq1996@outlook.com>
2022-03-06 18:07:56 +0330

    Add View class for detail wiew instead of function view Modify programming comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import (PostListView, SearchView, PostCategory,
-                    PostTag, post_detail_view, login_view,
+                    PostTag, login_view,
                     logout_view, PostCreateView, PostUpdateView,
-                    PostDeleteView, PostDraftListView)
+                    PostDeleteView, PostDraftListView, PostDetail)
 
 
 app_name = 'blog'
@@ -11,12 +11,13 @@ urlpatterns = [
     path('search/', SearchView.as_view(), name='search'),
     path('category/<slug:category>/', PostCategory.as_view(), name='category_view'),
     path('tags/<slug:tag>/', PostTag.as_view(), name='tag_view'),
-    path('blogs/<int:pk>/', post_detail_view, name='post_detail'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('create/', PostCreateView.as_view(), name='post_create'),
     path('update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
     path('delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
     path('draft/', PostDraftListView.as_view(), name='post_draft'),
+
+    path('post/<int:pk>/', PostDetail.as_view(), name='post_detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from .models import Post, Category, Tag, Blogger, Comment
 from .forms import CommentForm, PostForm
 from django.contrib.auth import authenticate, logout, login
@@ -8,30 +8,119 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 
 
-class PostCreateView(LoginRequiredMixin, CreateView):
-    login_url = '/login/'
-    redirect_field_name = 'blog/blog.html'
+class PostListView(ListView):
+    """
+        List of published posts
+        Display four posts per page
+        Displays all tags, categories, blog owner profiles, and the last three posts
+    """
     model = Post
-    form_class = PostForm
-    template_name = 'blog/create_post.html'
+    paginate_by = 4
+    template_name = 'blog/blog.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        posts = Post.objects.filter(published_at__isnull=False).order_by('-published_at')
+        return posts
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        context['categories'] = Category.objects.all()
+        context['recent'] = Post.objects.all().order_by('-published_at')[:3]
+        context['blogger'] = Blogger.objects.get(id=1)
+        return context
 
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
-    login_url = '/login/'
+class PostDetail(View):
+    """
+        Details about a particular post with the option to leave a comment
+        The superuser can edit and delete post on this page if they are logged in.
+    """
+    form_class = CommentForm
+    template_name = 'blog/single-blog.html'
 
-    form_class = PostForm
-    model = Post
-    template_name = 'blog/create_post.html'
+    def get(self, request, **kwargs):
+        context = dict()
+        context['post'] = Post.objects.get(id=self.kwargs['pk'])
+        context['tags'] = Tag.objects.all()
+        context['categories'] = Category.objects.all()
+        context['recent'] = Post.objects.all().order_by('-created_at')[:3]
+        context['blogger'] = Blogger.objects.get(id=1)
+        context['posts'] = Post.objects.all()
+        context['comments'] = Comment.objects.filter(status__exact='publish').order_by('-created_at')
+        return render(request, self.template_name, context)
+
+    def post(self, request, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            title = form.cleaned_data['title']
+            message = form.cleaned_data['message']
+            post = Post.objects.get(id=self.kwargs['pk'])
+            comment = Comment(post=post,
+                              name=name,
+                              email=email,
+                              title=title,
+                              message=message)
+            comment.save()
+            return redirect('blog:post_detail')
+        return render(request, self.template_name, {'error': 'ایمیل شما صحیح نمیباشد.'})
 
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
-    login_url = 'login'
-    model = Post
-    template_name = 'blog/post_delete_confirm.html'
-    success_url = reverse_lazy('blog:post_list')
+class PostCategory(PostListView):
+    """
+        This page inherits from 'PostListView' for elements such as tags, search box, etc.
+        Display posts containing specific category slug
+        Also, there is no pagination and all posts appear on one page
+
+    """
+    paginate_by = 0
+
+    def get_context_data(self, **kwargs):
+        context = super(PostCategory, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(category__slug=self.kwargs['category'])
+        return context
+
+
+class SearchView(PostListView):
+    """
+        This page inherits from 'PostListView' for elements such as tags, categories, etc.
+        Get information about search box and return posts whose titles contain search box information
+        Also, there is no pagination and all posts appear on one page
+
+    """
+    paginate_by = 0
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchView, self).get_context_data(**kwargs)
+        search = self.request.GET.get('search')
+        context['posts'] = Post.objects.filter(title__icontains=search)
+        return context
+
+
+class PostTag(PostListView):
+    """
+        This page inherits from 'PostListView' for elements such as tags, categories, etc.
+        Display posts containing specific tags slug
+        Also, there is no pagination and all posts appear on one page
+    """
+    paginate_by = 0
+
+    def get_context_data(self, **kwargs):
+        context = super(PostTag, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(tags__slug=self.kwargs['tag'])
+        return context
 
 
 def login_view(request):
+    """
+        If request is: 'get', login page only reload. Otherwise, request is 'post. then, it gets username and password
+            from login template form
+        If there was any user and that user was active, then user can log in. Otherwise, you can see the errors in
+            the relevant template.
+    """
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -52,115 +141,103 @@ def login_view(request):
 
 @login_required
 def logout_view(request):
+    """
+        Logout view function.
+        This simply uses the built-in 'logout' method then redirects to the post list page
+    """
     logout(request)
     return redirect('blog:post_list')
 
 
-class PostListView(ListView):
-    """
-    return descending(the newest post is first) list of all posts, tags, categories, 3 recent post, and admin profile.
-    post list page contain 4 posts
-    """
-    model = Post
-    paginate_by = 4
-    template_name = 'blog/blog.html'
-    context_object_name = 'posts'
-
-    def get_queryset(self):
-        # Return descending list of all posts
-        posts = Post.objects.filter(published_at__isnull=False).order_by('-published_at')
-        return posts
-
-    def get_context_data(self, **kwargs):
-        # Return tags, categories, 3 recent post, and admin profile.
-        context = super().get_context_data(**kwargs)
-        context['tags'] = Tag.objects.all()
-        context['categories'] = Category.objects.all()
-        context['recent'] = Post.objects.all().order_by('-published_at')[:3]
-        context['blogger'] = Blogger.objects.get(id=1)
-        return context
-
-
 class PostDraftListView(LoginRequiredMixin, PostListView):
+    """
+        List of Draft posts.
+        Login is required. If the user isn't logged in, it will automatically redirect to the login page.
+        After login, the user can see draft posts. Also publish, update or delete a post
+    """
     login_url = '/login/'
 
     def get_queryset(self):
         return Post.objects.filter(published_at__isnull=True).order_by('-created_at')
 
 
-class PostCategory(PostListView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     """
-    inherit from 'PostListView' for other elements of this page.
-    return posts that contain specific category slug
-    also, there is no paginator and all of the posts display on one single page
+        Creating new post
+        Login is required. If user isn't logged in, it will automatically redirect to the login page
+        After login superuser can add post
     """
-    paginate_by = 0
-
-    def get_context_data(self, **kwargs):
-        # Return posts that contain specific category slug
-        context = super(PostCategory, self).get_context_data(**kwargs)
-        context['posts'] = Post.objects.filter(category__slug=self.kwargs['category'])
-        return context
+    login_url = '/login/'
+    redirect_field_name = 'blog/blog.html'
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/create_post.html'
 
 
-class SearchView(PostListView):
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     """
-    Get search box information and return posts their title contains search box information
-    also, there is no paginator and all of the posts display on one single page
+        Updating posts
+        Login is required. If user isn't logged in, it will automatically redirect to the login page
+        After login superuser can edit posts
     """
-    paginate_by = 0
+    login_url = '/login/'
 
-    def get_context_data(self, **kwargs):
-        context = super(SearchView, self).get_context_data(**kwargs)
-        q = self.request.GET.get('search')
-        context['posts'] = Post.objects.filter(title__icontains=q)
-        return context
+    form_class = PostForm
+    model = Post
+    template_name = 'blog/create_post.html'
 
 
-class PostTag(PostListView):
+class PostDeleteView(LoginRequiredMixin, DeleteView):
     """
-    inherit from 'PostListView' for other elements of this page.
-    return posts that contain specific tags slug
-    also, there is no paginator and all of the posts display on one single page
+        Deleting posts
+        Login is required. If user isn't logged in, it will automatically redirect to the login page
+        After login superuser can delete posts
     """
-    paginate_by = 0
-
-    def get_context_data(self, **kwargs):
-        context = super(PostTag, self).get_context_data(**kwargs)
-        context['posts'] = Post.objects.filter(tags__slug=self.kwargs['tag'])
-        return context
+    login_url = 'login'
+    model = Post
+    template_name = 'blog/post_delete_confirm.html'
+    success_url = reverse_lazy('blog:post_list')
 
 
-def post_detail_view(request, pk):
-    """
-    Return specific post that specify by primary key and contain comment section in this page
-    Also, returns all tags, categories, 3 recent post, and admin profile.
-    """
-    context = dict()
-    context['post'] = Post.objects.get(id=pk)
-    context['tags'] = Tag.objects.all()
-    context['categories'] = Category.objects.all()
-    context['recent'] = Post.objects.all().order_by('-created_at')[:3]
-    context['blogger'] = Blogger.objects.get(id=1)
-    context['posts'] = Post.objects.all()
-    context['comments'] = Comment.objects.filter(status__exact='publish').order_by('-created_at')
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            title = form.cleaned_data['title']
-            message = form.cleaned_data['message']
-            comment = Comment(post=context['post'],
-                              name=name,
-                              email=email,
-                              title=title,
-                              message=message)
-            comment.save()
-
-    return render(request, 'blog/single-blog.html', context)
 
 
+
+
+
+
+
+
+
+
+
+# def post_detail_view(request, pk):
+#     """
+#     Return specific post that specify by primary key and contain comment section in this page
+#     Also, returns all tags, categories, 3 recent post, and admin profile.
+#     """
+#     context = dict()
+#     context['post'] = Post.objects.get(id=pk)
+#     context['tags'] = Tag.objects.all()
+#     context['categories'] = Category.objects.all()
+#     context['recent'] = Post.objects.all().order_by('-created_at')[:3]
+#     context['blogger'] = Blogger.objects.get(id=1)
+#     context['posts'] = Post.objects.all()
+#     context['comments'] = Comment.objects.filter(status__exact='publish').order_by('-created_at')
+#     if request.method == 'POST':
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             name = form.cleaned_data['name']
+#             email = form.cleaned_data['email']
+#             title = form.cleaned_data['title']
+#             message = form.cleaned_data['message']
+#             comment = Comment(post=context['post'],
+#                               name=name,
+#                               email=email,
+#                               title=title,
+#                               message=message)
+#             comment.save()
+#
+#     return render(request, 'blog/single-blog.html', context)
 
 
 
