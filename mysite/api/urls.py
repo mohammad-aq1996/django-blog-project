8130ba274700797c1612d9fcda807cee2703b256
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-04 18:13:28 +0430

    add Category, Tag, Comment view set and router

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
+
+
 app_name = 'api'
+
 router = routers.SimpleRouter()
 router.register('post', views.PostViewSet)
+router.register('tag', views.TagViewSet)
+router.register('category', views.CategoryViewSet)
+router.register('comment', views.CommentViewSet)
+
 urlpatterns = [
     path('', include(router.urls)),
 ]
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,8 +1,35 @@
 from rest_framework import viewsets
 from blog.models import Post, Category, Tag, Comment
-from .serializers import PostSerializer
+from .serializers import PostSerializer, CategorySerializer, TagSerializer, CommentCategory
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentCategory
+
+
+
+
+
+
+
+
+
+
+
+
