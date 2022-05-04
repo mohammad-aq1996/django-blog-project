1ce0b1e143384e0427e7948ea2533b371246c2bd
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-04 18:03:04 +0430

    add post view set and router

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 app_name = 'api'
-
+router = routers.SimpleRouter()
+router.register('post', views.PostViewSet)
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
