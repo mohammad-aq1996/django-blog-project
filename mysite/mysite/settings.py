fcf8f12effe19fb2471efd523b21eaec79eef48e
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-04 12:09:34 +0430

    add rest api settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -43,7 +44,14 @@ INSTALLED_APPS = [
     'ckeditor',
     'ckeditor_uploader',
     'api.apps.ApiConfig',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
