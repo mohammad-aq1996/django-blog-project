ca29d526de5a713ae23248246b479fb6111c9a64
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-04 18:13:20 +0430

    add Category, Tag, Comment serializer to api

--- mysite/api/serializers.py
+++ mysite/api/serializers.py
@@ -6,3 +6,21 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class CommentCategory(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
