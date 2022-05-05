f0d3441f19f9be1f6556c5e2c1d3f8e5f993c9d1
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-05 11:00:18 +0430

    add JWT to api

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
+
 from . import views
 
 
@@ -13,4 +15,7 @@ router.register('comment', views.CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -51,6 +51,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
 MIDDLEWARE = [
