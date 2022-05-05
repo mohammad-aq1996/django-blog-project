a8a862d2881f9bfb950943dd58a91ac8069e45ac
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-05 12:15:38 +0430

    add nested serializer to PostSerializer in api

--- mysite/api/serializers.py
+++ mysite/api/serializers.py
@@ -3,6 +3,19 @@ from blog.models import Post, Category, Tag, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
+    def get_author(self, obj):
+        return obj.author.username
+
+    def get_tag(self, obj):
+        return [tag.name for tag in obj.tags.all()]
+
+    def get_category(self, obj):
+        return obj.category.name
+
+    author = serializers.SerializerMethodField('get_author')
+    tags = serializers.SerializerMethodField('get_tag')
+    category = serializers.SerializerMethodField('get_category')
+
     class Meta:
         model = Post
         fields = '__all__'
