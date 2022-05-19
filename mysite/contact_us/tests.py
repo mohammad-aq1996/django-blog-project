6e3bd8536c3c941876855dd9bd34b5902a03fb8d
Mohammad Ali Gholampour <mohammad.aq1996@yahoo.com>
2022-05-19 10:29:39 +0430

    add test for comment in contact_us app

--- mysite/contact_us/tests.py
+++ mysite/contact_us/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Comment
 
-# Create your tests here.
+
+class TestComment(TestCase):
+    def setUp(self):
+        self.comment = Comment.objects.create(
+            name = 'mamali',
+            email = 'mamal@gmail.com',
+            subject = 'Good Idea',
+            message = "You\'r blog is awsome.",
+        )
+
+    def test_comment_listing(self):
+        self.assertEqual(f'{self.comment.name}', 'mamali')
+        self.assertEqual(f'{self.comment.email}', 'mamal@gmail.com')
+        self.assertEqual(f'{self.comment.subject}', 'Good Idea')
+        self.assertEqual(f'{self.comment.message}', "You\'r blog is awsome.")
+        
